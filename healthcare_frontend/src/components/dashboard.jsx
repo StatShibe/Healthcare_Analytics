78f48e9652141a80dac63ac796c21fd6ef4836a4
Haresh Moorthy <100016407+StatShibe@users.noreply.github.com>
2024-02-21 14:44:11 +0530

    created react frontend

---
+++ healthcare_frontend/src/components/dashboard.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+//  import ReactDom from "react-dom"
+
+const Navbar = () => {
+    return(
+        <>
+            <h1>Hello Theni</h1>
+        </>
+    );
+}
+
+export default Navbar;
