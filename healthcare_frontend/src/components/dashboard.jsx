38d8d1851948f9a45929ff422e52819bc7beacef
Haresh Moorthy <100016407+StatShibe@users.noreply.github.com>
2024-02-21 14:57:24 +0530

    added navbar

--- healthcare_frontend/src/components/dashboard.jsx
+++ healthcare_frontend/src/components/dashboard.jsx
@@ -1,12 +1,23 @@
 import React from "react";
+import Container from  'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 //  import ReactDom from "react-dom"
 
-const Navbar = () => {
+const Navigationbar = () => {
     return(
-        <>
-            <h1>Hello Theni</h1>
-        </>
+        <Navbar expand = "ld" className = "bg-body-tertiary">
+            <Container>
+                <Navbar.Brand href = "#home">React</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href = "#home">Home</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     );
 }
 
-export default Navbar;
+export default Navigationbar;
