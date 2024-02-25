ddd8b9c30842d973ba7d97ac256768d63df665ef
Haresh Moorthy <100016407+StatShibe@users.noreply.github.com>
2024-02-25 18:10:22 +0530

    added chart.js

--- healthcare_frontend/src/App.js
+++ healthcare_frontend/src/App.js
@@ -1,10 +1,27 @@
-import Navigationbar from "./components/dashboard"
+import Navigationbar from "./components/navigationbar"
+import Dashboard from "./components/dashboard";
 import './App.css';
 
+
+
 function App() {
+  const data = {
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+          label: '# of Votes',
+          data: [12, 19, 3, 5, 2, 3],
+          borderWidth: 1,
+          backgroundColor: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+          borderColor: "black"
+        }]
+  }
+  
   return (
     <div className="App">
       <Navigationbar/>
+      <Dashboard 
+        chartData={data}
+      />
     </div>
   );
 }
--- healthcare_frontend/src/components/dashboard.jsx
+++ healthcare_frontend/src/components/dashboard.jsx
@@ -1,32 +1,27 @@
 import React from "react";
-import Container from  'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import medlogo from '../assets/images/medlogo.jpg';
-import '../assets/styles/dashboard.css';
-//  import ReactDom from "react-dom"
-
-const Navigationbar = () => {
-    return(
-        <Navbar expand = "md" style={{backgroundColor : "#5b00ff", color : "white"}}>
-            <Container>
-                <Navbar.Brand href = "#home"><img src = {medlogo} alt = "Logo"/></Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav"/>
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
-                        <Nav.Link href = "#home" style={{color : "white"}}>Home</Nav.Link>
-                        <Nav.Link href = "#link" style={{color : 'white'}}>Link</Nav.Link>
-                        <NavDropdown title = "Dropdown" id = "basic-nav-dropdown" style={{color : 'white'}}>
-                            <NavDropdown.Item href="#action">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action2">Action2</NavDropdown.Item>
-                            <NavDropdown.Item href="#action3">Action3</NavDropdown.Item>
-                        </NavDropdown>
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
+import { Bar } from 'react-chartjs-2';
+import { Doughnut } from 'react-chartjs-2';
+import Row from "react-bootstrap/Row"
+import Col from "react-bootstrap/Col"
+import Container from "react-bootstrap/Container"
+import {Chart as ChartJS} from "chart.js/auto"
+const Dashboard = ({chartData}) => {
+    return (
+        <Container>
+            <Row>
+                <Col>
+                    <Bar
+                        data={chartData}
+                    />
+                </Col>
+                <Col>
+                    <Doughnut
+                        data={chartData}
+                    />
+                </Col>
+            </Row>
+        </Container>
     );
 }
 
-export default Navigationbar;
+export default Dashboard;
