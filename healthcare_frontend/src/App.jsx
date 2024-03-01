15931ecab278bc622fcad62c8c8a94d630239ca4
Haresh Moorthy <100016407+StatShibe@users.noreply.github.com>
2024-03-01 21:10:37 +0530

    created login page

--- healthcare_frontend/src/App.jsx
+++ healthcare_frontend/src/App.jsx
@@ -1,16 +1,18 @@
 import './App.css'
 import LoginForm from './components/LoginForm'
+import cardiogramIcon from "./assets/images/electrocardiogram.svg" 
 
 function App() {
 
   return (
-    <div className='flex w-full h-screen'>
-      <div className='w-full flex items-center justify-center lg:w-1/2'>
+    <div className="flex w-full h-screen bg-[url('./assets/images/loginbackground.png')] bg-auto">
+      <div className="w-full flex items-center justify-center lg:w-1/2">
         <LoginForm/>
       </div>
-      <div className='hidden relative lg:flex h-full w-1/2 items-center justify-center bg-gray-200'>
-        <div className='w-60 h-60 bg-gradient-to-tr from-violet-500 to-pink-500 rounded-full animate-bounce'/>
-        <div className='w-full h-1/2 absolute bottom-0 bg-white/10 backdrop-blur-lg'/>
+      <div className='hidden relative lg:flex h-full w-1/2 items-center justify-center'>
+        <div className='w-60 h-60 bg-gradient-to-tr from-violet-950 to-red-600 rounded-full'>
+          <img src={cardiogramIcon} alt='Some symbol' className='w-70 h-70 animate-pulse'/>
+        </div>
       </div>
     </div>
   )
--- healthcare_frontend/src/components/LoginForm.jsx
+++ healthcare_frontend/src/components/LoginForm.jsx
@@ -2,31 +2,31 @@ import React from "react";
 
 const LoginForm = () => {
     return(
-        <div className="bg-white px-10 py-20 rounded-3xl border-2 border-gray">
-            <h1 className="text-5xl font-semibold">Welcome Back</h1>
-            <p className="font-medium text-lg text-gray-500 mt-4">Welcome back! Please enter your details.</p>
-            <div className="mt-8">
-                <div>
+        <div className="bg-white px-10 py-30 rounded-3xl border-2 border-gray">
+            <h1 className="text-4xl font-semibold m-3 text-center mt-6">Welcome Back</h1>
+            <p className="font-medium text-lg text-gray-500 mt-4 text-center">Please enter your details.</p>
+            <div className="mt-6">
+                <div className="my-3">
                     <label className="text-lg font-medium">Email</label>
-                    <input className="w-full border-2 border-gray-100 rounded-xl p-4 mt-1 bg-transparent" placeholder="Enter your email"/>
+                    <input className="w-full border-2 border-gray-100 rounded-xl p-4 mt-1 bg-transparent" placeholder="Enter your email" type = "email"/>
                 </div>
                 <div>
                     <label className="text-lg font-medium">Password</label>
-                    <input className="w-full border-2 border-gray-100 rounded-xl p-4 mt-1 bg-transparent" placeholder="Enter your password"/>
+                    <input className="w-full border-2 border-gray-100 rounded-xl p-4 mt-1 bg-transparent" placeholder="Enter your password" type="password"/>
                 </div>
                 <div className="mt-8 flex justify-between items-center">
                     <div>
                         <input type = "checkbox" id = "remember"/>
                         <label className = "ml-2 font-medium text-base text-violet-400" for = "remember">Remember for 30 days</label>
                     </div>
-                    <button>Forgot Password</button>
+                    <button className="hover:text-purple-500">Forgot Password</button>
                 </div>
                 <div className="mt-8 flex flex-col gap-y-4">
                     <button className="hover: shadow-lg active:scale-[.98] active:duration-75 hover:scale-[1.01] ease-in-out transition-all py-3 rounded-xl bg-violet-500 text-white text-lg font-bold">Sign in</button>
                 </div>
-                <div className="mt-8 flex justify-center items-center">
+                <div className="mt-8 mb-4 flex justify-center items-center">
                     <p className="font-medium text-base">Don't have an account?</p>
-                    <button className="text-violet-500 text-base font-medium ml-2">Sign Up</button>
+                    <button className="text-violet-500 text-base font-medium ml-2" type="submit">Sign Up</button>
                 </div>
             </div>
         </div>
