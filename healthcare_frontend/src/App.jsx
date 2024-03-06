3545a34978b84ce4069a0e5a58f6fe92f742d82b
Haresh Moorthy <100016407+StatShibe@users.noreply.github.com>
2024-03-06 14:42:05 +0530

    changed login page into a component

--- healthcare_frontend/src/App.jsx
+++ healthcare_frontend/src/App.jsx
@@ -1,21 +1,11 @@
 import './App.css'
-import LoginForm from './components/LoginForm'
-import cardiogramIcon from "./assets/images/electrocardiogram.svg" 
-import cardiogramIcon2 from "./assets/images/electroheart.svg"
+
+import LoginPage from './pages/LoginPage'
 
 function App() {
 
   return (
-    <div className="flex w-full h-screen bg-[url('./assets/images/loginbackground.png')] bg-auto">
-      <div className="w-full flex items-center justify-center lg:w-1/2">
-        <LoginForm/>
-      </div>
-      <div className='hidden relative lg:flex h-full w-1/2 items-center justify-center'>
-        <div className='w-60 h-60 bg-gradient-to-tr from-violet-950 to-blue-400 rounded-full'>
-          <img src={cardiogramIcon} alt='Some symbol' className='w-70 h-70 animate-pulse'/>
-        </div>
-      </div>
-    </div>
+    <LoginPage />
   )
 }
 
--- healthcare_frontend/src/components/LoginForm.jsx
+++ healthcare_frontend/src/components/LoginForm.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 
 const LoginForm = () => {
+    const handleSubmit = () => {
+        console.log("Hello there!")
+    }
     return(
         <div className="bg-white px-10 py-30 rounded-3xl border-2 border-gray">
             <h1 className="text-4xl font-semibold m-3 text-center mt-6">Welcome Back</h1>
@@ -19,11 +22,11 @@ const LoginForm = () => {
                     <button className="hover:text-purple-500 underline">Forgot Password</button>
                 </div>
                 <div className="mt-8 flex flex-col gap-y-4">
-                    <button className="hover: shadow-lg active:scale-[.98] active:duration-75 hover:scale-[1.01] ease-in-out hover:bg-gradient-to-r from-violet-500 to-pink-400  transition-all py-3 rounded-xl bg-violet-500 text-white text-lg font-bold">Sign in</button>
+                    <input className="hover: shadow-lg active:scale-[.98] active:duration-75 hover:scale-[1.01] ease-in-out hover:bg-gradient-to-r from-violet-500 to-pink-400  transition-all py-3 rounded-xl bg-violet-500 text-white text-lg font-bold" type="submit" onClick={handleSubmit}/>
                 </div>
                 <div className="mt-8 mb-4 flex justify-center items-center">
                     <p className="font-medium text-base">Don't have an account?</p>
-                    <input className="text-violet-500 text-base font-medium ml-2" type="submit" value="Sign up"/>
+                    <button className="text-violet-500 text-base font-medium ml-2">Sign Up</button>
                 </div>
             </div>
             </form>
