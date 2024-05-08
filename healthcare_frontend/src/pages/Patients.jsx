488e5769277600a34be5f4625f98ea0710751fe2
Haresh Moorthy <100016407+StatShibe@users.noreply.github.com>
2024-05-08 21:58:02 +0530

    Added

--- healthcare_frontend/src/pages/Patients.jsx
+++ healthcare_frontend/src/pages/Patients.jsx
@@ -1,13 +1,19 @@
 import NavigationBar from "../components/NavigationBar"
 import SideMenu from "../components/SideMenu"
+import CardComp from "../components/CardComp"
 
 const Patients = () => {
+  const patientStyling = "m-6 w-[300px] h-fit"
+  const displayStyling = "m-6 w-[500px] h-fit"
   return (
     <div className="flex flex-row">
       <SideMenu/>
       <div className="h-screen w-screen bg-white font-poppins">
           <NavigationBar/>  
-          <h1>Patients</h1>
+          <div className="flex flex-row justify-around">
+              <CardComp text = {"Patients Search"} styling = {patientStyling}/>
+              <CardComp text = {"Patients Display"} styling = {displayStyling}/>
+          </div>
       </div>
     </div>
   )
--- healthcare_frontend/src/pages/RoomAllocation.jsx
+++ healthcare_frontend/src/pages/RoomAllocation.jsx
@@ -1,16 +1,27 @@
 import SideMenu from "../components/SideMenu"
 import NavigationBar from "../components/NavigationBar"
+import CardComp from "../components/CardComp"
+import { Car } from "lucide-react"
 
 const RoomAllocation = () => {
+  const cardStyling1 = "m-6 h-[120px] w-[180px] text-center"
+  const cardStyling2 = "m-6 h-[120px] w-[180px] text-center"
   return (
     <div className="flex flex-row">
       <SideMenu/>
       <div className="h-screen w-screen bg-white font-poppins">
           <NavigationBar/>
-          <h1>Room Allocation page</h1>
+          <div className="flex flex-row justify-left">
+            <CardComp text={"Vacant"} styling={cardStyling1}/>
+            <CardComp text={"Occupied"} styling={cardStyling2}/>
+          </div>
+          <div className="flex flex-row justify-around">
+            <CardComp text = {"Room check"} styling = {""}/>
+            <CardComp text = {"Room details"} styling = {""}/>
+          </div>
       </div>
     </div>
-  )
+  ) 
 }
 
 export default RoomAllocation
