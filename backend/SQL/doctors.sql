d51affcefea7e2e04c2c05cfffb7fd164062cce6
Hayagreevan V <hayagree@gmail.com>
2024-05-09 14:57:16 +0530

    Db Schema

--- backend/SQL/doctors.sql
+++ backend/SQL/doctors.sql
@@ -1,14 +1,20 @@
-CREATE TABLE public.doctors
+CREATE TABLE IF NOT EXISTS public.doctors
 (
-    doctor_id serial NOT NULL,
-    name character varying(50) NOT NULL,
-    email character varying(50) NOT NULL,
-    password character varying(200) NOT NULL,
-    specialization character varying(50),
-    availability character varying(50),
-    PRIMARY KEY (doctor_id),
-    UNIQUE (email)
-);
+    name character varying(50) COLLATE pg_catalog."default" NOT NULL,
+    email character varying(50) COLLATE pg_catalog."default" NOT NULL,
+    specialization character varying(50) COLLATE pg_catalog."default",
+    availability character varying(50) COLLATE pg_catalog."default",
+    user_id integer NOT NULL,
+    CONSTRAINT doctors_pkey PRIMARY KEY (email),
+    CONSTRAINT doctors_email_key UNIQUE (email),
+    CONSTRAINT doctors_user_id_key UNIQUE (user_id),
+    CONSTRAINT doctors_user_id_fkey FOREIGN KEY (user_id)
+        REFERENCES public.users (user_id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+)
+
+TABLESPACE pg_default;
 
 ALTER TABLE IF EXISTS public.doctors
     OWNER to postgres;
