1caddfb7224fbf10e16eb8e183ed0e98f0bb1d4c
Haresh Moorthy <100016407+StatShibe@users.noreply.github.com>
2024-05-19 08:25:26 +0530

    cleaning code

--- healthcare_frontend/src/pages/Appointments.jsx
+++ healthcare_frontend/src/pages/Appointments.jsx
@@ -1,15 +1,11 @@
 import SideMenu from '../components/SideMenu'
 import NavigationBar from '../components/NavigationBar'
-// import CardComp from '../components/CardComp'
 import { useEffect, useState } from 'react'
 import axios from 'axios'
 import { Card, CardContent } from '@mui/material'
 
 const Appointments = () => {
-  const calenderStyling = "m-6 h-[300px] w-[300px]"
   const listStyling = "m-6 h-screen w-[600px]"
-  const recencyStyling = "m-6 h-[300px] w-[300px]"
-
   const [data,setData] = useState([]);
 
   useEffect(()=>{
@@ -24,11 +20,7 @@ const Appointments = () => {
       <div className="h-screen w-screen bg-white font-poppins">
           <NavigationBar/>
           <div className='grid grid-cols-2 grid-rows-2 h-screen w-fit'>
-            <div>
-              {/* <CardComp text = {"Calender"} styling ={calenderStyling}/> */}
-            </div>
             <div className='col-row-2 w-[600px] h-fit'>
-              {/* <CardComp text = {"Appointment List"} styling = {listStyling}/> */}
               {
                 data?.map((row,index)=>{
                   return(
@@ -46,9 +38,6 @@ const Appointments = () => {
                   </Card>
                 )})
               }
-            </div>
-            <div>
-              {/* <CardComp text  = {"Recency List"} styling={recencyStyling}/> */}
             </div>
           </div>
       </div>
